0480312caf9c55431d45cff5c763907bbd26004e
Carlos Santana <carlos.santana.gacitua@gmail.com>
2025-02-03 11:50:22 -0300

    Subo challenge a GitHub

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var listaAmigos = [];
+let inputHTML = document.getElementById("amigo");
+
+function agregarAmigo() {
+    const nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo === "") {
+        alert("Debe escribir un nombre");
+        return;
+    }
+
+    console.log(typeof(nombreAmigo));
+    console.log(`Amigo: ${nombreAmigo}`);
+  
+    listaAmigos.push(nombreAmigo)
+
+    // Crear un nuevo <li>
+    const nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = nombreAmigo;
+    // Agregar el <li> a la lista <ul>
+    document.getElementById("listaAmigos").appendChild(nuevoElemento);
+    
+    // Limpiar el input después de agregar
+    inputHTML.value = "";
+    return
+  }
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+    
+    const generaNumero = Math.floor(Math.random()*(listaAmigos.length));
+    console.log(`Número generado: ${generaNumero}`);
+
+   // Crear un nuevo <li>
+    const nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = listaAmigos[generaNumero];
+    // Agregar el <li> al resultado <ul>
+    document.getElementById("resultado").appendChild(nuevoElemento);
+
+    // Deshabilita botones - Fin
+    document.getElementById("botonAgrega").setAttribute("disabled", true)
+    document.getElementById("botonSortea").setAttribute("disabled", true)
+
+    return
+  }
